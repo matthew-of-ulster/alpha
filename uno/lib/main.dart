11ea30bc4e78d2eb37404ed66eba814fc044eca9
Matthew <matthewhwallace17@gmail.com>
2024-07-01 20:46:54 +0100

    dialog close button adds text entry to database

--- uno/lib/main.dart
+++ uno/lib/main.dart
@@ -18,21 +18,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo 2',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -55,7 +40,21 @@ Future getDBResults() async {
   List<TodoItem> allItems = await database.select(database.todoItems).get();
 
   return allItems;
-  //print('items in database: $allItems');
+}
+
+void addDBResult(String title) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = AppDatabase();
+
+  if (title.length < 6) {
+    title = title + "aaaaaa";
+  }
+
+  await database.into(database.todoItems).insert(TodoItemsCompanion.insert(
+        title: title,
+        content: 'null content',
+      ));
 }
 
 class MyHomePage extends StatefulWidget {
@@ -145,12 +144,14 @@ class _MyHomePageState extends State<MyHomePage> {
           content: TextField(
             controller: taskController,
             decoration: const InputDecoration(
-              hintText: 'Enter some text',
+              hintText: 'Enter text to add',
             ),
           ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
+                addDBResult(taskController.text);
+                setState(() {});
                 Navigator.of(context).pop();
               },
               child: const Text('Close'),
